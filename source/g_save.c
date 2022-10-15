6ea7e1feb015d1bf5b99320d361fc92e285cc0fa
m4son <mrsima@gmail.com>
2022-10-16 00:27:32 +0200

    loud_guns should be 0 by default

--- source/g_save.c
+++ source/g_save.c
@@ -427,7 +427,7 @@ void InitGame( void )
 	empty_rotate = gi.cvar( "empty_rotate", "0", 0 );
 	empty_exec = gi.cvar( "empty_exec", "", 0 );
 	llsound = gi.cvar( "llsound", "0", 0 );
-	loud_guns = gi.cvar( "loud_guns", "1", 0 );
+	loud_guns = gi.cvar( "loud_guns", "0", 0 );
 	sync_guns = gi.cvar( "sync_guns", "1", 0 );
 	silentwalk = gi.cvar( "silentwalk", "0", 0 );
 	slopefix = gi.cvar( "slopefix", "1", 0 );
