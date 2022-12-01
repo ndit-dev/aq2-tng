c03dda3fe6dfa588082202132bcbf42282911e8c
adean <8dino2@gmail.com>
2022-12-01 15:29:05 -0500

    Removed commented and removed references

--- source/g_spawn.c
+++ source/g_spawn.c
@@ -233,9 +233,6 @@ void SP_point_combat (edict_t * self);
 void SP_misc_explobox (edict_t * self);
 void SP_misc_banner (edict_t * self);
 void SP_misc_satellite_dish (edict_t * self);
-void SP_misc_gib_arm (edict_t * self);
-void SP_misc_gib_leg (edict_t * self);
-void SP_misc_gib_head (edict_t * self);
 void SP_misc_deadsoldier (edict_t * self);
 void SP_misc_viper (edict_t * self);
 void SP_misc_viper_bomb (edict_t * self);
@@ -244,9 +241,6 @@ void SP_misc_strogg_ship (edict_t * self);
 void SP_misc_teleporter (edict_t * self);
 void SP_misc_teleporter_dest (edict_t * self);
 void SP_misc_blackhole (edict_t * self);
-void SP_misc_eastertank (edict_t * self);
-void SP_misc_easterchick (edict_t * self);
-void SP_misc_easterchick2 (edict_t * self);
 
 
 //zucc - item replacement function
@@ -267,14 +261,11 @@ static const spawn_t spawns[] = {
   {"item_health_small", SP_item_health_small},
   {"item_health_large", SP_item_health_large},
   {"item_health_mega", SP_item_health_mega},
-
   {"info_player_start", SP_info_player_start},
   {"info_player_deathmatch", SP_info_player_deathmatch},
   {"info_player_intermission", SP_info_player_intermission},
-
   {"info_player_team1", SP_info_player_team1},
   {"info_player_team2", SP_info_player_team2},
-
   {"func_plat", SP_func_plat},
   {"func_button", SP_func_button},
   {"func_door", SP_func_door},
@@ -291,7 +282,6 @@ static const spawn_t spawns[] = {
   {"func_timer", SP_func_timer},
   {"func_explosive", SP_func_explosive},
   {"func_killbox", SP_func_killbox},
-
   {"trigger_always", SP_trigger_always},
   {"trigger_once", SP_trigger_once},
   {"trigger_multiple", SP_trigger_multiple},
@@ -303,51 +293,31 @@ static const spawn_t spawns[] = {
   {"trigger_elevator", SP_trigger_elevator},
   {"trigger_gravity", SP_trigger_gravity},
   {"trigger_monsterjump", SP_trigger_monsterjump},
-
   {"target_temp_entity", SP_target_temp_entity},
   {"target_speaker", SP_target_speaker},
   {"target_explosion", SP_target_explosion},
   {"target_changelevel", SP_target_changelevel},
-//  {"target_secret", SP_target_secret},
-//  {"target_goal", SP_target_goal},
   {"target_splash", SP_target_splash},
   {"target_spawner", SP_target_spawner},
   {"target_blaster", SP_target_blaster},
   {"target_crosslevel_trigger", SP_target_crosslevel_trigger},
   {"target_crosslevel_target", SP_target_crosslevel_target},
   {"target_laser", SP_target_laser},
-//  {"target_help", SP_target_help},
-// monster      {"target_actor", SP_target_actor},
-//  {"target_lightramp", SP_target_lightramp},
   {"target_earthquake", SP_target_earthquake},
   {"target_character", SP_target_character},
   {"target_string", SP_target_string},
-
   {"worldspawn", SP_worldspawn},
   {"viewthing", SP_viewthing},
-
-//  {"light", SP_light},
   {"light_mine1", SP_light_mine1},
   {"light_mine2", SP_light_mine2},
   {"info_null", SP_info_null},
   {"func_group", SP_info_null},
   {"info_notnull", SP_info_notnull},
   {"path_corner", SP_path_corner},
-//  {"point_combat", SP_point_combat},
-
-//  {"misc_explobox", SP_misc_explobox},
   {"misc_banner", SP_misc_banner},
-
   {"misc_ctf_banner", SP_misc_ctf_banner},
   {"misc_ctf_small_banner", SP_misc_ctf_small_banner},
-
   {"misc_satellite_dish", SP_misc_satellite_dish},
-  // monster {"misc_actor", SP_misc_actor},
-  {"misc_gib_arm", SP_misc_gib_arm},
-  {"misc_gib_leg", SP_misc_gib_leg},
-  {"misc_gib_head", SP_misc_gib_head},
-  // monster {"misc_insane", SP_misc_insane},
-  //{"misc_deadsoldier", SP_misc_deadsoldier},
   {"misc_viper", SP_misc_viper},
   {"misc_viper_bomb", SP_misc_viper_bomb},
   {"misc_bigviper", SP_misc_bigviper},
@@ -357,9 +327,6 @@ static const spawn_t spawns[] = {
   {"trigger_teleport", SP_trigger_teleport},
   {"info_teleport_destination", SP_info_teleport_destination},
   {"misc_blackhole", SP_misc_blackhole},
-  {"misc_eastertank", SP_misc_eastertank},
-  {"misc_easterchick", SP_misc_easterchick},
-  {"misc_easterchick2", SP_misc_easterchick2},
 
   {NULL, NULL}
 };
