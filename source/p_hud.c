66b27fa473373459542b5701c11f267bb85ffff9
Aaron Dean <8dino2@gmail.com>
2024-01-08 12:11:31 -0500

    Change player icon if player is dead in GHUD

--- source/p_hud.c
+++ source/p_hud.c
@@ -1005,7 +1005,9 @@ void HUD_SpectatorUpdate(edict_t *clent)
 			Ghud_SetText(clent, hud[h + 2], nm_s);
 			Ghud_SetText(clent, hud[h + 3], kdr_s);
 
-			if (cl->curr_weap)
+			if (!IS_ALIVE(cl_ent))
+				Ghud_SetInt(clent, hud[h + 4], level.pic_health);
+			else if (cl->curr_weap)
 				Ghud_SetInt(clent, hud[h + 4], level.pic_items[cl->curr_weap]);
 			else
 				Ghud_SetInt(clent, hud[h + 4], level.pic_items[MK23_NUM]);
